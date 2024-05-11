3a0df3b38bcf7ca0d2125375ac1c08a4b94a1f57
MarwaWalid09 <163915897+MarwaWalid09@users.noreply.github.com>
2024-05-11 20:46:00 +0300

    Add files via upload

---
+++ front end/php/nav/navbar.php
@@ -0,0 +1,20 @@
+ <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+          <div class="container">
+            <a class="navbar-brand fs-4" href="#">
+              <span style="color:#ef8273;">Appointment</span>System</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="custom_nav-container" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="P_inertion.html">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="patient_appointments.html">Appointments</a>
+                </li>
+                
+              </ul>
+            </div>
+          </div>
+        </nav>
